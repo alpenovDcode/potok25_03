42be268b846f88aa14b3705230350b0d69475fe7
indiiira <indira_shakirova2003@mail.ru>
2024-04-25 22:10:31 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+my_string = input("Введите строку: ")
+print(len(my_string))
+my_string="нижний регистр"
+print ("my_string".upper())
+print ("my_string".lower())
+print ("нижний регистр".replace(' ',""))
